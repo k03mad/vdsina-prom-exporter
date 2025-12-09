838ccda8631e6e58804081396185d28507070c8e
k03mad <k03.mad@gmail.com>
2025-12-09 13:52:30 +0300

    import json

--- app/server.js
+++ app/server.js
@@ -1,12 +1,12 @@
 import {startMetricsServer} from '@k03mad/simple-prom';
 
 import env from '../env.js';
+import pkg from '../package.json' with {type: 'json'};
 
-import {packageJson} from './helpers/parse.js';
 import * as metrics from './metrics/_index.js';
 
 startMetricsServer({
-    appName: packageJson.name,
+    appName: pkg.name,
     port: env.server.port,
     debug: env.debug,
     metrics,
